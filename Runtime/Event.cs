7fa4194b5d7a8e52aa23a36cd21ddf21fe17c504
Alvaro <alsern01@ucm.es>
2021-07-01 14:44:29 +0200

    tracker class, event class, writer class

---
+++ Runtime/Event.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UAJ.Tracker
+{
+    /// <summary>
+    /// Base class for all the tracker events. Contains shared attributes
+    /// and functionality
+    /// </summary>
+    class Event
+    {
+        public float timeStamp;
+
+        public int playerID;
+
+        protected string path;
+
+        public Event()
+        {
+        }
+
+        public Event(int playerID)
+        {
+            Init (playerID);
+        }
+
+        public virtual void Init(int playerID)
+        {
+            this.playerID = playerID;
+        }
+
+        public virtual void UpdateInfo(float timeStamp)
+        {
+            this.timeStamp = timeStamp;
+        }
+
+        public string GetPath()
+        {
+            return path;
+        }
+    }
+}
---
+++ Runtime/Writer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace UAJ.Tracker
+{
+    public class Writer
+    {
+        private static string dataPath = null;
+
+        private static System.IO.StreamWriter _writer;
+
+        public static void Init()
+        {
+            dataPath = Application.persistentDataPath;
+        }
+
+        /// <summary>
+        /// Method used to parse and write event info into a .json file
+        /// </summary>
+        /// <param name="e">Event we want to parse and write to a file</param>
+        public static void WriteToFile(Event e)
+        {
+            _writer =
+                new System.IO.StreamWriter(dataPath + "/" + e.GetPath(), true);
+            _writer.WriteLine(JsonUtility.ToJson(e));
+            _writer.Close();
+        }
+    }
+}
